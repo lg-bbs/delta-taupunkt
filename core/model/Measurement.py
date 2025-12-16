dd4d5a5a7bd864630b1234148be0edb2208dfc1d
lg-bbs <lukas.digital10@gmail.com>
2025-12-16 08:21:35 +0100

    measurement

---
+++ core/model/Measurement.py
@@ -0,0 +1,4 @@
+class Measurement:
+    def __init__(self, temperatur: float, luftfeuchtigkeit: float):
+        self.temperatur = temperatur
+        self.luftfeuchtigkeit = luftfeuchtigkeit
---
+++ core/sensor/DHT.py
@@ -0,0 +1,24 @@
+import RPi.GPIO as GPIO
+import dht11
+
+from model.Measurement import Measurement
+
+class DHT:
+    def __init__(self, pin: int):
+        self.pin = pin
+
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BCM)
+
+        self.sensor = dht11.DHT11(pin=self.pin)
+
+    def measure(self) -> Measurement | None:
+        result = self.sensor.read()
+
+        if result.is_valid():
+            return Measurement(
+                temperatur = result.temperature,
+                luftfeuchtigkeit = result.humidity
+            )
+
+        return None
