9929a70d1b8e96d856bb09236f276fd5cb3f9fa5
lg-bbs <lukas.digital10@gmail.com>
2026-01-06 09:14:56 +0100

    self

--- core/model/Measurement.py
+++ core/model/Measurement.py
@@ -7,4 +7,4 @@ class Measurement:
         self.outside = outside
         
     def txt(self) -> str:
-        return f"{time}: Innen ({indide.temp}°C, {inside.hum})"
+        return f"{self.time}: Innen ({self.inside.temp}°C, {self.inside.hum})"
