282edc24959fa02e949fdf3eb965b3b8804f1b62
lg-bbs <lukas.digital10@gmail.com>
2026-01-20 08:12:35 +0100

    update Measurement.py

--- core/model/Measurement.py
+++ core/model/Measurement.py
@@ -7,4 +7,7 @@ class Measurement:
         self.outside = outside
         
     def txt(self) -> str:
+        if (!self.inside) {
+            return f"{self.time}: Keine Daten"
+        }
         return f"{self.time}: Innen ({self.inside.txt()})"
