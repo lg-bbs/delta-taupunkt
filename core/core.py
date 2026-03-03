521b2714a8385544a0b211c865be98bfa3a1fd4d
lg-bbs <lukas.digital10@gmail.com>
2026-03-03 09:13:15 +0100

    datenbank insert und erstellung basic calc (nur datei)

--- core/core.py
+++ core/core.py
@@ -1,11 +1,13 @@
 # core.py
 import time
 from .sensor.DHTHelper import DHTHelper
+from .db.MeasureDB import MeasureDB
 from .sensor.LCD import LCD
 
 def core(interval: int = 2):
     dhtHelper = DHTHelper()
     lcd = LCD()
+    db = MeasureDB()
     
     while True:
         m = dhtHelper.measure_all()
@@ -13,6 +15,7 @@ def core(interval: int = 2):
         if m:
             print(m.txt())
             lcd.showData(m)
+            db.insert_measurement(m)
         else:
             print("Messung ungültig")
 
