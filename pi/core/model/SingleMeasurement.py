1e3866653865bc41708f9271230382f50be86f57
lg-bbs <lukas.digital10@gmail.com>
2026-05-05 08:57:53 +0200

    update SingleMeasurement.py

--- pi/core/model/SingleMeasurement.py
+++ pi/core/model/SingleMeasurement.py
@@ -1,5 +1,5 @@
 class SingleMeasurement:
-    def __init__(self, temp: float, hum: float, dew: float):
+    def __init__(self, temp: float, hum: float, dew: float | None = None):
         self.temp = temp
         self.hum = hum
         self.dew = dew
