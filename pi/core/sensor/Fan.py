a43bb8982077bc9b857f0779bcd9ce7852de7029
lg-bbs <lukas.digital10@gmail.com>
2026-05-05 08:55:04 +0200

    update Fan.py

--- pi/core/sensor/Fan.py
+++ pi/core/sensor/Fan.py
@@ -3,8 +3,8 @@ import RPi.GPIO as GPIO
 
 class Fan:
     def __init__(self):
-        self.relay_pin = 40 
-        GPIO.setmode(GPIO.BOARD)
+        self.relay_pin = 21 
+        GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.relay_pin, GPIO.OUT)
 
     def turn_on(self):
