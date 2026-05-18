2a0536a3f38863b561e0fbb9af42ba79dca67d11
LG-Programmer <84402671+LG-Programmer@users.noreply.github.com>
2026-05-18 21:01:53 +0200

    Fan servo fix

--- pi/core/sensor/Fan.py
+++ pi/core/sensor/Fan.py
@@ -6,9 +6,18 @@ class Fan:
         self.relay_pin = 21 
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.relay_pin, GPIO.OUT)
+        # Servo
+        self.servo_pin = 25 #TODO BCM 25 (BOARD 22), BCM 26 (BOARD 37)
+        GPIO.setup(self.servo_pin, GPIO.OUT)
+        self.servo_pwm = GPIO.PWM(self.servo_pin, 50)  # 50 Hz
 
     def turn_on(self):
         GPIO.output(self.relay_pin, GPIO.LOW)
+        #Servo
+        self.servo_pwm.start(0)
+        self.servo_pwm.ChangeDutyCycle(7.5) #TODO speed
 
     def turn_off(self):
         GPIO.output(self.relay_pin, GPIO.HIGH)
+        #Servo
+        self.servo_pwm.stop()
